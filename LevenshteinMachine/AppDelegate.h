7af20736fc03bb40d3009cf74aa4554687a77f6c
Moshe Berman <moshberm@gmail.com>
2014-11-06 00:24:04 -0500

    Initial Commit

---
+++ LevenshteinMachine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LevenshteinMachine
+//
+//  Created by Moshe on 11/6/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
